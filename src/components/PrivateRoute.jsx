a9d07b8f0dfc48ef119619cb63d3d9d31ffe0b7c
asif <masifabid@outlook.com>
2022-04-18 14:48:37 +0530

    authFinal

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import { useAuthProvider } from "./authProvider";
-import { Routes, Route, Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 export const PrivateRoute = () => {
   const { state } = useAuthProvider();
