bbae92ff696848abdad7f84429c8664f35097b1e
asif <masifabid@outlook.com>
2022-06-05 12:20:02 +0530

    postCall function added in reusable functions

--- src/components/ReusableFunctions.jsx
+++ src/components/ReusableFunctions.jsx
@@ -11,3 +11,15 @@ export const getCall = async (endPoint) => {
     return response.data;
   }
 };
+
+export const postCall = async (endPoint, requestBody) => {
+  const token = localStorage.getItem("encodedToken");
+  const response = await axios.post(endPoint, requestBody, {
+    headers: {
+      authorization: token,
+    },
+  });
+  if (response.status === 201 || response.status === 200) {
+    return response.data;
+  }
+};
