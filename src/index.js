54ee0667f6e0a6ba241876e2568b8669d96078d7
Mohd Asif <masifabid@outlook.com>
2022-07-18 15:15:29 +0530

    removed context

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
-import { ProductProvider } from "./components/productProvider";
 import {BrowserRouter as Router} from "react-router-dom";
-import { AuthProvider } from "./components/authProvider";
 import {Provider} from "react-redux"
 import { store } from "./features/store";
 
@@ -14,15 +12,11 @@ makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthProvider>
-    <ProductProvider>
     <Provider store={store}>
       <Router>
       <App />
       </Router>
       </Provider>
-    </ProductProvider>
-    </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
