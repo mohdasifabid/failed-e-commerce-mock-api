3f829fd43fd777e74cd66f63243cd58b3fa6907d
Mohd Asif <masifabid@outlook.com>
2022-07-29 17:44:07 +0530

    fixed: login problem

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
     if (token) {
       reduxDispatch(setAuthentication(true))
     } else {
-      reduxDispatch(setAuthentication(true))
+      reduxDispatch(setAuthentication(false))
     }
   }, []);
 
